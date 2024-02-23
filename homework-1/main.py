5d2cef565acd6711c510fe48e357232142dfd559
koipish <koipish@gmail.com>
2024-02-23 12:49:38 +0300

    added code

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,36 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="123")
+with open('north_data/employees_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    with conn.cursor() as cur:
+        for row in reader:
+            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s,%s, %s)",(row))
+        cur.execute("SELECT * FROM employees")
+        conn.commit()
+        rows = cur.fetchall()
+
+
+with open('north_data/customers_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    with conn.cursor() as cur:
+        for row in reader:
+            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)",(row))
+        cur.execute("SELECT * FROM customers")
+        conn.commit()
+        rows = cur.fetchall()
+
+with open('north_data/orders_data.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    with conn.cursor() as cur:
+        for row in reader:
+            cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",(row))
+        cur.execute("SELECT * FROM orders")
+        conn.commit()
+        rows = cur.fetchall()
+conn.close()
